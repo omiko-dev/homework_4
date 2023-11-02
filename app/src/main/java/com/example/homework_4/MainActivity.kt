1c2a31440fc9658d1b2cbf7377a2eef96b1e9405
omiko-dev <omikovalishvili0@gmail.com>
2023-11-02 18:51:05 +0400

    homework 4 update

--- app/src/main/java/com/example/homework_4/MainActivity.kt
+++ app/src/main/java/com/example/homework_4/MainActivity.kt
@@ -2,16 +2,10 @@ package com.example.homework_4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.homework_4.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
     }
 }
